bb5c24b7fd3e2c3ae9b4658a4ab38e46eaa9218c
Media Man <matt.zuckermann@my.wheaton.edu>
2018-10-31 22:33:48 -0700

    First Master Pull

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// git checkout -B [branch-name] :: to make a new branch
+// git checkout [branch name] :: to switch branches
+// git merge [branch name] :: to merge [branch name] into the current branch you're on from "git checkout"
+// git status [shows what needs to be done]
+// git push origin [branch-name] :: to 
+
+// code . in order to open up in vs code
+// cmd+k is to clear out terminal
+//lgtm "looks good to me"; used sometimes in pull requests
+//MVP: "minimum viable product"
+//
